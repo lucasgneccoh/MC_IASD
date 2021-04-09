b6fd5598106b1130412e08dc806853b4fea70fee
lucasgneccoh <61275066+lucasgneccoh@users.noreply.github.com>
2021-04-09 19:48:47 +0200

    added json file for tournaments

---
+++ bot_fights/README.md
@@ -0,0 +1,3 @@
+See the example file and follow the syntax. You can run multiple tournaments with one file. 
+The "bot" attribute must always be present and correspond to a valid bot key (See play_functions for the dictionary of available bots)
+Be careful with the attributes you pass to each bot. For the moment, passing wrong arguments (or extra arguments) results in an error. The match will still run, but without results.
--- modules/tournament.py
+++ modules/tournament.py
@@ -10,6 +10,7 @@ import os
 import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from datetime import datetime
+import json
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # %% Set transposition table info here
@@ -23,6 +24,8 @@ T.T_Table.White = GAME.White
 T.T_Table.Black = GAME.Black
 
 
+bot_dict = PLAYERS.bots
+
 # %% Classes and functions
 
 def parseInputs():
@@ -31,7 +34,8 @@ def parseInputs():
   parser.add_argument("--verbose", help="Controls console output. true or false", default='false')
   parser.add_argument("--save_at_each", help="Choose to save the results after every match. If false, saves only at the end of the tournament", default='true')
   parser.add_argument("--rounds", help="Number of rounds each pair of bots will play (two games per round, home and away)", default=2)
-  parser.add_argument("--n", help="Budget given to the algorithms that use a budget", default=100)
+  bot_options = ', '.join(bot_dict.keys())
+  parser.add_argument("--bots", help=f"JSON file containing the tournaments and the bots involved. The options for the bots are {bot_options}. See \bot_fights\tournament_example.json for an example.", default="../bot_fights/tournament_example.json")
   args = parser.parse_args()
   return args
 
@@ -72,6 +76,60 @@ def res_status(res, white_bot, black_bot):
   return "error"
 
 
+def bot_from_json_dict(info, bot_dict):
+  name = '_'.join(list(map(str, info.values())))
+  bot = info['bot']
+  del info['bot']
+  return Bot(name, bot_dict[bot], **info)
+  
+
+# Fichier bots.csv avec les elos et les wins
+def bots_df(BASE_PATH, all_bots):
+  path_b = os.path.join(BASE_PATH,"bots.csv")
+  if not os.path.exists(path_b):
+    with open(path_b,'w+') as f:
+      f.close()
+    df = pd.DataFrame(columns = ["bot", "elo", "nb_played"] + [bot.name for bot in all_bots])
+    for i, bot in enumerate(all_bots):
+        df.loc[i] = [bot.name, 1200, 0] + ["0/0"]*len(all_bots)
+    df.set_index("bot", inplace=True)
+    if not os.path.exists(path_b):
+      with open(path_b,'w+') as f:
+        f.close()
+    df.to_csv(path_b)
+  else:
+    try:
+        df = pd.read_csv(path_b, index_col="bot")
+        for b in all_bots:
+            if not b in df.columns:
+                raise Exception('New bot. Reseting table')
+    except:
+        df = pd.DataFrame(columns = ["bot", "elo", "nb_played"] + [bot.name for bot in all_bots])
+        for i, bot in enumerate(all_bots):
+            df.loc[i] = [bot.name, 1200, 0] + ["0/0"]*len(all_bots)
+        df.set_index("bot", inplace=True)
+        df.to_csv(path_b)
+  return df
+
+# Fichier matches_history.csv avec l'historique des parties
+def history_df(BASE_PATH):  
+  path_h = os.path.join(BASE_PATH,"matches_history.csv")
+  if not os.path.exists(path_h):
+    with open(path_h, 'w+') as f:
+      f.close()
+    df_hist = pd.DataFrame(columns = ["datetime", "White", "Black", "Winner", "Elo White Before", "Elo Black Before", "Elo White After","Elo Black After"])
+    df_hist.to_csv(path_h)
+  else:
+    try:
+        df_hist = pd.read_csv(path_h)    
+    except:
+        df_hist = pd.DataFrame(columns = ["datetime", "White", "Black", "Winner", "Elo White Before", "Elo Black Before", "Elo White After","Elo Black After"])
+        df_hist.to_csv(path_h)
+  return df_hist
+
+
+
+
 # %% Parse arguments
 args = parseInputs()
 
@@ -81,150 +139,98 @@ SAVE_EACH = True if args.save_at_each=='true'else False
 ROUNDS = int(args.rounds)
 PRINT_LENGTH = 40
 FILL_CHAR = '-'
-n_tot = int(args.n)
-
-# %% Define bots
-# shu1000_c4 = Bot("SHUSS_1000_C4", PLAYERS.SHUSS, n=n_tot, c = 4)
-shu1000_c16 = Bot("SHUSS_1000_C16", PLAYERS.SHUSS, n=n_tot, c = 16)
-shu1000_c64 = Bot("SHUSS_1000_C64", PLAYERS.SHUSS, n=n_tot, c = 64)
-shu1000_c128 = Bot("SHUSS_1000_C128", PLAYERS.SHUSS, n=n_tot, c = 128)
-shu1000_c256 = Bot("SHUSS_1000_C256", PLAYERS.SHUSS, n=n_tot, c = 256)
-
-uct1000 = Bot("UCT_1000", PLAYERS.BestMoveUCT, n=n_tot)
-sh1000 = Bot("SH_1000", PLAYERS.SequentialHalving, n=n_tot)
-gr1000 = Bot("GRAVE_1000", PLAYERS.BestMoveGRAVE, n=n_tot)
-r1000 = Bot("RAVE_1000", PLAYERS.BestMoveRAVE, n=n_tot)
-ucb1000 = Bot("UCB_1000", PLAYERS.UCB, n=n_tot)
-flat1000 = Bot("FLAT_1000", PLAYERS.flat, n=n_tot)
+TOURNAMENT_PATH = args.bots
 
 
-#Controls
-dumb = Bot("Dumb", PLAYERS.same_move)
-random = Bot("Random", PLAYERS.random_bot)
-
-all_bots = [shu1000_c16, shu1000_c64, shu1000_c128, shu1000_c256, uct1000, sh1000, gr1000, r1000, ucb1000, flat1000]
-
-# Compare only the SHUSS
-# all_bots = [shu1000_c16, shu1000_c64, shu1000_c128, shu1000_c256, random]
-
-
-# %% For testing
-# ucbTest= Bot("UCB_test", PLAYERS.UCB, n=100)
-# flatTest = Bot("FLAT_test", PLAYERS.flat, n=100)
-# all_bots = [ucbTest, flatTest]
-
-# %% Fichier bots.csv avec les elos et les wins
-
-
-path_b = os.path.join(BASE_PATH,"bots.csv")
-if not os.path.exists(path_b):
-  with open(path_b,'w+') as f:
-    f.close()
-  df = pd.DataFrame(columns = ["bot", "elo", "nb_played"] + [bot.name for bot in all_bots])
-  for i, bot in enumerate(all_bots):
-      df.loc[i] = [bot.name, 1200, 0] + ["0/0"]*len(all_bots)
-  df.set_index("bot", inplace=True)
-  if not os.path.exists(path_b):
-    with open(path_b,'w+') as f:
-      f.close()
-  df.to_csv(path_b)
-else:
-  try:
-      df = pd.read_csv(path_b, index_col="bot")
-      for b in all_bots:
-          if not b in df.columns:
-              raise Exception('New bot. Reseting table')
-  except:
-      df = pd.DataFrame(columns = ["bot", "elo", "nb_played"] + [bot.name for bot in all_bots])
-      for i, bot in enumerate(all_bots):
-          df.loc[i] = [bot.name, 1200, 0] + ["0/0"]*len(all_bots)
-      df.set_index("bot", inplace=True)
-      df.to_csv(path_b)
-
-# %% Fichier matches_history.csv avec l'historique des parties
-
-path_h = os.path.join(BASE_PATH,"matches_history.csv")
-if not os.path.exists(path_h):
-  with open(path_h, 'w+') as f:
-    f.close()
-  df_hist = pd.DataFrame(columns = ["datetime", "White", "Black", "Winner", "Elo White Before", "Elo Black Before", "Elo White After","Elo Black After"])
-  df_hist.to_csv(path_h)
-else:
-  try:
-      df_hist = pd.read_csv(path_h)    
-  except:
-      df_hist = pd.DataFrame(columns = ["datetime", "White", "Black", "Winner", "Elo White Before", "Elo Black Before", "Elo White After","Elo Black After"])
-      df_hist.to_csv(path_h)
+# %% Read json file with the tournaments to run
+with open(TOURNAMENT_PATH) as f:
+  content = json.load(f)
+tournaments = content['tournaments']
 
 # %% main
 
-all_matches = list(combinations(all_bots, 2))
+path_b = os.path.join(BASE_PATH,"bots.csv")
+path_h = os.path.join(BASE_PATH,"matches_history.csv")
 
-simple_table = pd.DataFrame(columns = ['white_bot','black_bot', 'score','time'])
-
-dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-format_key = "{{:{}^{}}}".format(FILL_CHAR, PRINT_LENGTH)
-format_key_star = "{{:{}^{}}}".format("*", PRINT_LENGTH)
-counter = 0
-for r in range(ROUNDS):
-    shuffle(all_matches)
-    if VERBOSE:
-      print("*"*PRINT_LENGTH)
-      print(format_key_star.format(" ROUND {} ".format(r)))
-      print("*"*PRINT_LENGTH)
-    for i in range(len(all_matches)):
-        white_bot, black_bot = all_matches[i][0], all_matches[i][1]
-        if VERBOSE:          
-          print(format_key.format("  MATCH {} on {}  ".format(i + 1, len(all_matches))))  
-          print("-"*PRINT_LENGTH)
-          print(format_key.format("  MATCH {} VS {}  ".format(white_bot.name, black_bot.name)))
+for tourn in tournaments:
+  format_key = "{{:{}^{}}}".format(FILL_CHAR, PRINT_LENGTH)
+  format_key_star = "{{:{}^{}}}".format("*", PRINT_LENGTH)
+  
+  
+  print("*"*PRINT_LENGTH)
+  print(format_key_star.format(" TOURNAMENT {} ".format(tourn)))
+  print("*"*PRINT_LENGTH)
+  print()
+  all_bots = [bot_from_json_dict(t, bot_dict) for t in tournaments[tourn]]
+  all_matches = list(combinations(all_bots, 2))
+  
+  df = bots_df(BASE_PATH, all_bots)
+  df_hist = history_df(BASE_PATH)
+  
+  simple_table = pd.DataFrame(columns = ['white_bot','black_bot', 'score','time'])
+  
+  dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+  
+  counter = 0
+  for r in range(ROUNDS):
+      shuffle(all_matches)
+      if VERBOSE:
+        print("*"*PRINT_LENGTH)
+        print(format_key_star.format(" ROUND {} ".format(r+1)))
+        print("*"*PRINT_LENGTH)
+      for i in range(len(all_matches)):
+          white_bot, black_bot = all_matches[i][0], all_matches[i][1]
+          if VERBOSE:          
+            print(format_key.format("  MATCH {} on {}  ".format(i + 1, len(all_matches))))  
+            print("-"*PRINT_LENGTH)
+            print(format_key.format("  MATCH {} VS {}  ".format(white_bot.name, black_bot.name)))
+            
+          start = time.process_time()
+          try:
+            res = bot1_vs_bot2(white_bot = white_bot, black_bot = black_bot, verbose = VERBOSE)
+          except Exception as e:
+            print(e)
+            res = None
           
-        start = time.process_time()
-        try:
-          res = bot1_vs_bot2(white_bot = white_bot, black_bot = black_bot, verbose = VERBOSE)
-        except Exception as e:
-          print(e)
-          res = None
+          simple_table.loc[counter] = [white_bot.name, black_bot.name, res, time.process_time()-start]
+          counter += 1
+          if VERBOSE: 
+            print("\nWinner: {}".format(res_status(res,white_bot,black_bot)))
+          
+          hist = elo.updateTable(df, df_hist, white_bot, black_bot, res, dt_string)
+          df_hist = df_hist.append(hist, ignore_index = True)
+          white_bot, black_bot = all_matches[i][1], all_matches[i][0]
+          print()
+          if VERBOSE: 
+            print(format_key.format("  SWITCHING SIDES  "))
+          start = time.process_time()
+          try:
+            res = bot1_vs_bot2(white_bot = white_bot, black_bot = black_bot, verbose = VERBOSE)
+          except Exception as e:
+            print(e)
+            res = None
+          
+          
+          simple_table.loc[counter] = [white_bot.name, black_bot.name, res, time.process_time()-start]
+          counter += 1
+          if VERBOSE: 
+            print("\nWinner: {}".format(res_status(res,white_bot,black_bot)))
         
-        simple_table.loc[counter] = [white_bot.name, black_bot.name, res, time.process_time()-start]
-        counter += 1
-        if VERBOSE: 
-          print("\rWinner: {}".format(res_status(res,white_bot,black_bot)))
-        
-        hist = elo.updateTable(df, df_hist, white_bot, black_bot, res, dt_string)
-        df_hist = df_hist.append(hist, ignore_index = True)
-        white_bot, black_bot = all_matches[i][1], all_matches[i][0]
-        print()
-        if VERBOSE: 
-          print(format_key.format("  SWITCHING SIDES  "))
-        start = time.process_time()
-        try:
-          res = bot1_vs_bot2(white_bot = white_bot, black_bot = black_bot, verbose = VERBOSE)
-        except Exception as e:
-          print(e)
-          res = None
-        
-        
-        simple_table.loc[counter] = [white_bot.name, black_bot.name, res, time.process_time()-start]
-        counter += 1
-        if VERBOSE: 
-          print("\rWinner: {}".format(res_status(res,white_bot,black_bot)))
-      
-        hist = elo.updateTable(df, df_hist, white_bot, black_bot, res, dt_string)
-        df_hist = df_hist.append(hist, ignore_index = True)
-        
-        if SAVE_EACH:
-          df.to_csv(path_b)
-          df_hist.to_csv(path_h, index = False)
-        
-
-df.to_csv(path_b)
-df_hist.to_csv(path_h, index = False)
-path = os.path.join(BASE_PATH,"simple_table_{}.csv".format(dt_string))
-if not os.path.exists(path):
-  with open(path,'w') as f:
-    f.close()
-simple_table.to_csv(path, index = False)
-
-print(f"Tournament is over. Results are available in {BASE_PATH}")
+          hist = elo.updateTable(df, df_hist, white_bot, black_bot, res, dt_string)
+          df_hist = df_hist.append(hist, ignore_index = True)
+          
+          if SAVE_EACH:
+            df.to_csv(path_b)
+            df_hist.to_csv(path_h, index = False)
+          
+  
+  df.to_csv(path_b)
+  df_hist.to_csv(path_h, index = False)
+  path = os.path.join(BASE_PATH,"simple_table_{}.csv".format(dt_string))
+  if not os.path.exists(path):
+    with open(path,'w') as f:
+      f.close()
+  simple_table.to_csv(path, index = False)
+  
+  print(f"Tournament {tourn} is over. Results are available in {BASE_PATH}")
 
